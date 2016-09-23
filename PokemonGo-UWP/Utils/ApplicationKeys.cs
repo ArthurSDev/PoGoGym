00bc57cc1c4197591cd1878cf9b4c6792a4249b7
ArthurSDev <tech.windoows@hotmail.com>
2016-09-22 22:29:50 -0300

    Create ApplicationKeys.cs

---
+++ PokemonGo-UWP/Utils/ApplicationKeys.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonGo_UWP.Utils
+{
+    public static class ApplicationKeys
+    {
+        public static string MapServiceToken = "7H7lMjEkAfP3PeOrrPVO~IRTU1f4lP6GTdpBxi4gqoQ~AvvbAnSGbHtsowQ98zRfwvaw6PdCgo2vq3x75R3_SbvN2zb7-YcaM_UIPNtNWOWK";
+        public static string HockeyAppToken = null;
+        public static string[] MapBoxTokens = {};
+        public static string[] MapBoxStylesLight = {};
+        public static string[] MapBoxStylesDark = {};
+    }
+}
